261e3e8a9b1d89596f605a4a70388fb84e860b1a
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-16 14:45:43 -0300

    corrige ortografia do método cumprimentar
    cria atributo filhos
    
    cria método biografia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,21 @@
 class Pessoa:
-    def __init__(self, nome, idade=None):
+    def __init__(self, nome, idade=None, *filhos):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
-    def cunprimentar(self):
+    def cumprimentar(self):
         return f"Olá, {self.nome}"
+
+    def biografia(self):
+        print(f"Nome: [{self.nome}]")
+        print(f"Idade: [{self.idade}]")
+        print("Filhos: ", end="")
+        for filho in self.filhos:
+            print(f"[{filho}] ", end="")
 
 
 if __name__ == "__main__":
-    p = Pessoa("Felipe")
-    print(p.cunprimentar())
+    felipe = Pessoa("Felipe", 35, *["Nina", "Mel"])
+    print(felipe.cumprimentar())
+    felipe.biografia()
