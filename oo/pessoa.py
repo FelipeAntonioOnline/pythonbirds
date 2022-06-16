b68a91acefff9b15f27ce4e659e90f063e0b2ad6
Felipe Antonio <felipe.antonio.online@proton.me>
2022-06-16 15:03:29 -0300

    criado atributo de classe olhos.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, nome, idade=None, *filhos):
         self.nome = nome
         self.idade = idade
@@ -13,9 +15,12 @@ class Pessoa:
         print("Filhos: ", end="")
         for filho in self.filhos:
             print(f"[{filho}] ", end="")
+        print("")
 
 
 if __name__ == "__main__":
     felipe = Pessoa("Felipe", 35, *["Nina", "Mel"])
     print(felipe.cumprimentar())
     felipe.biografia()
+    print(f"ID de Felipe: {id(felipe.olhos)}")
+    print(f"ID de Pessoa: {id(Pessoa.olhos)}")
